8f3120000e628983c5d1eb4b5393418b7c93bd13
Kaylane <kaycoutinho387@gmail.com>
2024-09-25 11:58:42 -0300

    Importação de funções de outros arquivos

---
+++ funcoes.py
@@ -0,0 +1,8 @@
+def soma(n1,n2):
+    return n1 + n2
+
+def somalista(lista):
+    rs = 0
+    for i in lista:
+        rs+=i 
+    print(f"O resultado da soma é {rs}")
---
+++ usar_funcoes.py
@@ -0,0 +1,5 @@
+#import funcoes as fn
+from funcoes import soma as sm
+s = sm(10,5)
+print(f"O resultado é {s}")
+
